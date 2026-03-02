fca0aafe073aeb2e0aefe32924f41860d8557316
amritanshusingh97 <singh.amritanshu1997@gmail.com>
2026-03-03 03:52:36 +0530

    frst commit

---
+++ src/main/java/com/youtubechannel/bitandbytes0101/Bitandbytes0101Application.java
@@ -0,0 +1,13 @@
+package com.youtubechannel.bitandbytes0101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bitandbytes0101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bitandbytes0101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/youtubechannel/bitandbytes0101/Bitandbytes0101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtubechannel.bitandbytes0101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bitandbytes0101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
